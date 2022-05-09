a2fe5e9bde027caf3fa4653cc5f1e83a5701b813
akari <terrorblade047@gmail.com>
2022-05-09 01:38:02 -0700

    all the small things

---
+++ app.js
@@ -0,0 +1,44 @@
+// let arrayResult = "";
+// function returnNum (){
+//     const arrayNum = [5, 10, 15];
+//     const result = Math.floor(Math.random() * arrayNum.length);
+//     arrayResult += result;
+// }
+// console.log(arrayResult);
+
+// const game = ["Rock", "Paper", "Scissors"];
+
+
+// function computerPlay () {
+//     let randomNumber = Math.floor(Math.random() * game.length);
+//     console.log(randomNumber);
+// }
+
+
+// let arrayNum = [5, 15, 25];
+
+// arrayNum.forEach(computerPlay);
+
+// function computerPlay () {
+//     let result = Math.floor(Math.random() * arrayNum.length);
+//     console.log(result);
+// }
+
+// const myArray = ["Rock", "Paper", "Scissors"];
+
+// function computerPlay() {
+//     return myArray[~~(Math.random() * myArray.length)];
+//   }
+  
+
+// function computerPlay () {
+//     let randomNumber = Math.floor(Math.random() * 3)
+//     switch(randomNumber) {
+//         case 0:
+//             return "Rock";
+//         case 1:
+//             return "Paper";
+//         case 2: 
+//             return "Scissors";
+//     }
+// }
